0946f2f39f3ada4f6e3e406f2303b8fcce24a35b
yxoni <psy337250@gmail.com>
2024-11-05 13:35:39 +0900

    sbb 3-02 페이징 기능 추가하기

--- sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ sbb/src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -4,6 +4,7 @@ import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.answer.AnswerRepository;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +20,19 @@ import java.util.Optional;
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired
+	private QuestionService questionService;
+
+	@Test
+	void testJpa() {
+		for (int i = 0; i < 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content);
+		}
+	}
+
+	/*
 	@Autowired
 	private QuestionRepository questionRepository;
 
@@ -129,7 +143,5 @@ class SbbApplicationTests {
 		Answer a = oa.get();
 		assertEquals(2, a.getQuestion().getId());
 	}
-
-
-
+	*/
  }
